ce284dafa0beec08733c6aad866d91da97fbb97a
Hack Reactor Students <accounts@hackreactor.com>
2017-01-03 13:19:09 -0800

    With this commit I intend to: -Add messages to the chat box via AJAX calls

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,22 +1,18 @@
 // YOUR CODE HERE:
 var app = {
- 
-  server: 'https://api.parse.com/1/classes/messages' 
-
+  server: 'https://api.parse.com/1/classes/messages'
 };
 
 app.init = function() {
-
+  app.fetch();
 };
 
 app.send = function(message) {
-
   // var message = {
   //   username: 'Marcus',
   //   text: 'Happy new year!',
   //   roomname: 'Fantasy suite'
   // };
-
   $.ajax({
     // This is the url you should use to communicate with the parse API server.
     url: 'https://api.parse.com/1/classes/messages',
@@ -32,36 +28,37 @@ app.send = function(message) {
     }
   });
 };
+app.renderMessage = function(message) { 
+  $('#chats').append('<li> username: ' + message.username + ' message: ' + message.text + ' roomname: ' + message.roomname + '</li>');
+};
 
 app.fetch = function() {
   $.ajax({
-  // This is the url you should use to communicate with the parse API server.
+    // This is the url you should use to communicate with the parse API server.
     url: 'https://api.parse.com/1/classes/messages',
     type: 'GET',
-    //data: 'json',
-    //contentType: 'application/json',
-    // success: displayMessage 
-    // error: function (data) {
-    //   // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-    //   console.error('chatterbox: Failed to send message', data);
-    // }
+    contentType: 'application/json',
+    success: function (data) {
+      data.results.forEach(function(item, index) {
+        debugger;
+        app.renderMessage(item);
+      });
+    },
+    error: function (data) {
+      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to send message', data);
+    }
   });
-
 };
 
 app.clearMessages = function() {
   $('#chats').empty();
 };
 
-app.renderMessage = function(message) { 
-  // console.log("mess: ",message);
- // console.log("chat: ", $('#chats'));
-  $('div #chats').append('<li> username: ' + message.username + ' message: ' + message.text + ' roomname: ' + message.roomname + '</li>');
-  // console.log($chats);
-};
 
 app.renderRoom = function (room) {
-
-//console.log(room);
   $('div #roomSelect').append('<li> Room: ' + room);
 };
+
+//$('.submit').on('click', app.send($('#message').val()))
+app.init();
